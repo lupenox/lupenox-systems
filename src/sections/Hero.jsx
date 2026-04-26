339454b6681fa7404e40cd830a14e3bccddfff7a
lupenox <logan.lupenox@gmail.com>
2026-04-25 19:22:15 -0500

    added background for hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,14 +1,19 @@
 import "../styles/sections/hero.css";
 import "../styles/sections/sections.css";
+import VeilBG from "../components/Animated_Background";
 
 function Hero() {
   return (
     <section className="hero-section">
-        <div className="container">
-            <h1 className="hero-title">Logan Lapierre</h1>
-            <p className="hero-subtitle">DevOps • Linux • Automation</p>
-        </div>
-    </section>
+      <div className="hero-bg">
+        <VeilBG />
+    </div>
+
+    <div className="container hero-content">
+      <h1 className="hero-title">Logan Lapierre</h1>
+      <p className="hero-subtitle">DevOps • Linux • Automation</p>
+   </div>
+</section>
   );
 }
 
