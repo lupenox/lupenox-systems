4cb6088a8a018d50f30f24ef90d3224f6aaf7e8e
lupenox <logan.lupenox@gmail.com>
2026-05-07 15:23:00 -0500

    added content to about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,12 +1,61 @@
+import profilePic from "../assets/profile-pic.jpg";
 import "../styles/sections/sections.css";
+import "../styles/sections/about.css";
 
 function About() {
+  
   return (
-    <section id="about" className="about-section">
-        <h2>About Me</h2>
-        <p>Brief intro about who you are and what you focus on.</p>
-    </section>
+<section className="about-section">
+
+  <div className="about-container">
+
+    <div className="about-image">
+      <img src={profilePic} alt="Logan Lapierre" id="Me"/>
+    </div>
+
+    <div className="about-content">
+      <p className="about-tag">
+        COMPUTER SCIENCE STUDENT • DEVOPS FOCUSED
+      </p>
+
+      <h2>About Me</h2>
+
+      <div className="text"> 
+      <p>
+        Hello, I'm Logan Lapierre, a senior at the University of
+        Wisconsin–Milwaukee studying Computer Science.
+      </p>
+
+      <p>
+        My interests include Linux, DevOps, CI/CD, automation,
+        scripting, cloud technologies, and backend development.
+        I enjoy building tools and applications that improve
+        workflows, experimenting with AI-driven projects, and
+        modding video games.
+      </p>
+
+      <p>
+        I'm especially passionate about infrastructure-focused
+        engineering and enjoy solving technical problems,
+        debugging systems, and learning new technologies.
+        I'm currently pursuing opportunities in areas such as
+        DevOps, Site Reliability Engineering (SRE), Linux System
+        Administration, cloud support, and backend engineering.
+      </p>
+
+      <p>
+        I'm highly motivated, adaptable, and always looking for
+        opportunities to grow both technically and professionally.
+      </p>
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
   );
+
 }
 
 export default About;
