628195810a9c7c88a3a606d9c171407dee95e014
Logan <lupenoxwolf@gmail.com>
2026-06-21 14:09:14 -0500

    Remove magic frames from skills

--- src/components/Skill_Display.jsx
+++ src/components/Skill_Display.jsx
@@ -1,11 +1,11 @@
 function SkillDisplay({ category }) {
     return (
-      <div className="skill-card magic-border magic-starfield">
+      <div className="skill-card">
         <h3>{category.title}</h3>
   
         <div className="skill-pill-container">
           {category.items.map((skill) => (
-            <span className="skill-pill magic-pill" key={skill.id}>
+            <span className="skill-pill" key={skill.id}>
               {skill.icon && <span className="skill-icon">{skill.icon}</span>}
               {skill.name}
             </span>
