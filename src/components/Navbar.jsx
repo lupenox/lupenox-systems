71b1ef88cedf22583487704ff2e5968981d1401b
Logan <lupenoxwolf@gmail.com>
2026-06-21 06:06:47 -0500

    Add CV link to navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,7 @@ function Navbar() {
           <a href="#projects">Projects</a>
           <a href="#experience">Experience</a>
           <a href="#contact">Contact</a>
+          <a className="nav-cv" href="/logan-lapierre-cv.pdf">CV</a>
         </div>
       </div>
     </nav>
